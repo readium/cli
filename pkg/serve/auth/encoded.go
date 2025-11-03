51aba6b504b7dcc0fdd4a5d0dedb61fe3c060d6e
Henry <chocolatkey@gmail.com>
2025-11-03 13:22:17 -0800

    use http status constants

--- pkg/serve/auth/encoded.go
+++ pkg/serve/auth/encoded.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"encoding/base64"
 	"fmt"
+	"net/http"
 )
 
 type B64EncodedAuthProvider struct{}
@@ -10,9 +11,9 @@ type B64EncodedAuthProvider struct{}
 func (n *B64EncodedAuthProvider) Validate(token string) (string, int, error) {
 	path, err := base64.RawURLEncoding.DecodeString(token)
 	if err != nil {
-		return "", 400, fmt.Errorf("invalid base64url path: %w", err)
+		return "", http.StatusBadRequest, fmt.Errorf("invalid base64url path: %w", err)
 	}
-	return string(path), 200, nil
+	return string(path), http.StatusOK, nil
 }
 
 func NewB64EncodedAuthProvider() *B64EncodedAuthProvider {
